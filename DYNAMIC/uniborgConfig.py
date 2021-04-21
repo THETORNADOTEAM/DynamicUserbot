1c502963340cd7330812434fa370a8b7b41665c9
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-21 09:38:35 +0530

    Update uniborgConfig.py

--- DYNAMIC/uniborgConfig.py
+++ DYNAMIC/uniborgConfig.py
@@ -92,6 +92,8 @@ if ENV:
         #Google Chrome Stuff
         CHROME_DRIVER = os.environ.get("CHROME_DRIVER", "/app/.chromedriver/bin/chromedriver")
         GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", "/app/.apt/usr/bin/google-chrome")
+        # number of rows of buttons to be displayed in .helpme command
+        NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD = int(os.environ.get("NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD", 7))
         # Google Drive ()
         G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
         G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
