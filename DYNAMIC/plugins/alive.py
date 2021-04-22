b343f3eab6e3142f6509b85c8c0e6c4c99ea2993
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-22 13:29:27 +0530

    Update alive.py

--- DYNAMIC/plugins/alive.py
+++ DYNAMIC/plugins/alive.py
@@ -61,5 +61,5 @@ async def amireallyalive(yes):
 
     
     """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+    await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
     await alive.delete()
