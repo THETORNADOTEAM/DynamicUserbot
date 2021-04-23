8162850b019ab75ce4d08f0d6ee3a46506cd678a
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-23 10:56:17 +0530

    Update x.py

--- DYNAMIC/plugins/x.py
+++ DYNAMIC/plugins/x.py
@@ -90,7 +90,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             result = builder.article("© DYNAMIC BOT Userbot Help",text="{}\nCurrently Loaded Plugins: {}".format(query, len(CMD_LIST)),buttons=dc,link_preview=False)
             await event.answer([result] if result else None)
         else:
-              reply_pop_up_alert = "Please get your own Userbot😁😁,for more info visit @teamishere! 😎😎"
+              reply_pop_up_alert = "Please get your own Userbot,for more info visit @DYNAMICUSERBOTSUPPORT! "
               await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"helpme_next\((.+?)\)")
