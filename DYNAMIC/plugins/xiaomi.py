e8dc277e0a0ba85d3e55783934ff1bcc047253f8
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-23 09:03:54 +0530

    Create xiaomi.py

---
+++ DYNAMIC/plugins/xiaomi.py
@@ -0,0 +1,196 @@
+# created by @eve_enryu
+# edited & fix by @Jisan7509
+
+
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+
+@borg.on(admin_cmd(pattern="firmware(?: |$)(.*)"))
+@borg.on(sudo_cmd(pattern="firmware(?: |$)(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    firmware = f"firmware"
+    DYNAMICevent = await edit_or_reply(event, "```Processing```")
+    async with event.client.conversation("@XiaomiGeeksBot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=774181428)
+            )
+            await conv.send_message(f"/{firmware} {link}")
+            respond = await response
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await DYNAMICevent.edit("```Unblock @XiaomiGeeksBot plox```")
+            return
+        else:
+            await DYNAMICevent.delete()
+            await event.client.forward_messages(event.chat_id, respond.message)
+
+
+@borg.on(admin_cmd(pattern="vendor(?: |$)(.*)"))
+@borg.on(sudo_cmd(pattern="vendor(?: |$)(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    vendor = f"vendor"
+    DYNAMICevent = await edit_or_reply(event, "```Processing```")
+    async with event.client.conversation("@XiaomiGeeksBot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=774181428)
+            )
+            await conv.send_message(f"/{vendor} {link}")
+            respond = await response
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await DYNAMICevent.edit("```Unblock @XiaomiGeeksBot plox```")
+            return
+        else:
+            await DYNAMICevent.delete()
+            await event.client.forward_messages(event.chat_id, respond.message)
+
+
+@borg.on(admin_cmd(pattern="specs(?: |$)(.*)"))
+@borg.on(sudo_cmd(pattern="specs(?: |$)(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    specs = f"specs"
+    DYNAMICevent = await edit_or_reply(event, "```Processing```")
+    async with event.client.conversation("@XiaomiGeeksBot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=774181428)
+            )
+            await conv.send_message(f"/{specs} {link}")
+            respond = await response
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await DYNAMICevent.edit("```Unblock @XiaomiGeeksBot plox```")
+            return
+        else:
+            await DYNAMICevent.delete()
+            await event.client.forward_messages(event.chat_id, respond.message)
+
+
+@borg.on(admin_cmd(pattern="fastboot(?: |$)(.*)"))
+@borg.on(sudo_cmd(pattern="fastboot(?: |$)(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    fboot = f"fastboot"
+    DYNAMICevent = await edit_or_reply(event, "```Processing```")
+    async with event.client.conversation("@XiaomiGeeksBot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=774181428)
+            )
+            await conv.send_message(f"/{fboot} {link}")
+            respond = await response
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await DYNAMICevent.edit("```Unblock @XiaomiGeeksBot plox```")
+            return
+        else:
+            await DYNAMICevent.delete()
+            await event.client.forward_messages(event.chat_id, respond.message)
+
+
+@borg.on(admin_cmd(pattern="recovery(?: |$)(.*)"))
+@borg.on(sudo_cmd(pattern="recovery(?: |$)(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    recovery = f"recovery"
+    DYNAMICevent = await edit_or_reply(event, "```Processing```")
+    async with event.client.conversation("@XiaomiGeeksBot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=774181428)
+            )
+            await conv.send_message(f"/{recovery} {link}")
+            respond = await response
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await DYNAMICevent.edit("```Unblock @XiaomiGeeksBot plox```")
+            return
+        else:
+            await DYNAMICevent.delete()
+            await event.client.forward_messages(event.chat_id, respond.message)
+
+
+@borg.on(admin_cmd(pattern="pb(?: |$)(.*)"))
+@borg.on(sudo_cmd(pattern="pb(?: |$)(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    pitch = f"pb"
+    DYNAMICevent = await edit_or_reply(event, "```Processing```")
+    async with event.client.conversation("@XiaomiGeeksBot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=774181428)
+            )
+            await conv.send_message(f"/{pitch} {link}")
+            respond = await response
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await DYNAMICevent.edit("```Unblock @XiaomiGeeksBot plox```")
+            return
+        else:
+            await DYNAMICevent.delete()
+            await event.client.forward_messages(event.chat_id, respond.message)
+
+
+@borg.on(admin_cmd(pattern="of(?: |$)(.*)"))
+@borg.on(sudo_cmd(pattern="of(?: |$)(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    ofox = f"of"
+    DYNAMICevent = await edit_or_reply(event, "```Processing```")
+    async with event.client.conversation("@XiaomiGeeksBot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=774181428)
+            )
+            await conv.send_message(f"/{ofox} {link}")
+            respond = await response
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await DYNAMICevent.edit("```Unblock @XiaomiGeeksBot plox```")
+            return
+        else:
+            await DYNAMICevent.delete()
+            await event.client.forward_messages(event.chat_id, respond.message)
+
+
+CMD_HELP.update(
+    {
+        "xiaomi": "**Plugin :** `Xiaomi`\
+        \n\n__**For Xiaomeme devices only!**__\
+        \n\n**Syntax :** `.firmware` (codename)\
+        \n**Function : **Get lastest Firmware\
+        \n\n**Syntax :** `.vendor` (codename)\
+        \n**Function : **Get lastest Vendor\
+        \n\n**Syntax :** `.pb` (codename)\
+        \n**Function : **Get latest PBRP\
+        \n\n**Syntax :** `.specs` (codename)\
+        \n**Function : **Get quick spec information about device\
+        \n\n**Syntax :** `.fastboot` (codename)\
+        \n**Function : **Get latest fastboot MIUI\
+        \n\n**Syntax :** `.recovery` (codename)\
+        \n**Function : **Get latest recovery MIUI\
+        \n\n**Syntax :** `.of` (codename)\
+        \n**Function : **Get latest ORangeFox Recovery"
+    }
+)
