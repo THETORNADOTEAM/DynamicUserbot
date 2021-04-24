0f17f609cd7e87cb0322e5d591c748709e7276c8
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-24 08:43:53 +0530

    Update execmod.py

--- DYNAMIC/plugins/execmod.py
+++ DYNAMIC/plugins/execmod.py
@@ -11,7 +11,7 @@ import time
 import os
 import sys
 from telethon import events, functions, __version__
-from uniborg.utils import admin_cmd
+from DYNAMIC.utils import admin_cmd
 from asyncio.subprocess import PIPE as asyncPIPE
 from asyncio import create_subprocess_exec as asyncrunapp
 
