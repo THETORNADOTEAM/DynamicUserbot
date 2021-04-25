612dc317d5352f8c7437f69bc8c47ff4785d814f
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-25 15:57:23 +0530

    DYNAMIC ASSISTANT

---
+++ DYNAMIC/GODBOYX.py
@@ -0,0 +1,40 @@
+# COPYRIGHT (C) 2021 TEAM-DYNAMIC
+#MADE BY @LEGENDXOP 
+#MODIFIED BY @GODBOYX
+#KANG KARNA HAI TO CREDITS KE SAATH KARO OTHERWISE MAA CHOD DUNGA TUMHARI THEEKAI NA BHOSD WAALO
+import asyncio
+import os
+try:
+  from pyrogram import Client, idle
+except:
+  os.system("pip install pyrogram>=1.1.13")
+  from pyrogram import Client, idle
+
+import asyncio
+from userbot.utils import admin_cmd as godboy
+from userbot import bot as GODBOY
+API_ID = os.environ.get("APP_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+from telethon import events, custom, Button, TelegramClient
+import time
+from userbot import botnickname, ALIVE_NAME, bot
+token = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
+xbot = TelegramClient("godboy", API_ID, API_HASH).start(bot_token=token)
+pbot = Client("GODBOY", api_id=API_ID, api_hash=API_HASH, bot_token=token)
+BOT = str(botnickname) if botnickname else "DYNAMIC"
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "DYNAMIC USER"
+PHOTO = os.environ.get("ALIVE_PHOTO", None)
+GODBOYX = "[GODBOYX](https://t.me/GODBOYX)"
+VERSION = "1.1.0"
+ID = 1742906647, 1737497310
+REPO = "[Dynamic-Userbot](https://github.com/TeamDynamic/Dynamic-Userbot)"
+PRO = bot.uid
+devs = 1742906647
+MASTER = f"[{NAME}](tg://user?id={PRO})"
+GROUP = "[SUPPORT GROUP](https://t.me/DYNAMICUSERBOTSUPPORT)"
+if __name__=="__main__":
+  xbot.run_until_disconnected()
+
+if __name__=="__main__":
+  pbot.start()
+  run()
