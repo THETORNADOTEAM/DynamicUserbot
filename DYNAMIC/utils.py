0575bf4612d3b016acfd5940c9782a13096b9496
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-26 10:09:10 +0530

    Update utils.py

--- DYNAMIC/utils.py
+++ DYNAMIC/utils.py
@@ -19,7 +19,7 @@ from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 from var import Var
 
 from DYNAMIC import CMD_LIST, LOAD_PLUG, LOGS, SUDO_LIST, bot
-from DYNAMIC.GODBOYX import xbot
+from DYNAMIC.GODBOYX.py import xbot
 from DYNAMIC.helpers.exceptions import CancelProcess
 
 ENV = bool(os.environ.get("ENV", False))
