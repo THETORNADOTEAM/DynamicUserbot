67f6a45714d45907eed03602f13037b174ce9729
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-28 11:12:11 +0530

    Update bot.py

--- DYNAMIC/plugins/bot.py
+++ DYNAMIC/plugins/bot.py
@@ -26,7 +26,7 @@ from platform import python_version as pyver
 import heroku3
 import requests
 from git import Repo
-from DYNAMIC import util
+from DYNAMIC import utils
 from telethon import __version__
 from telethon.errors.rpcerrorlist import ChatSendMediaForbiddenError
 
