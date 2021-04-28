dd92f5c79ca8d55a10e06032a352f5c76be2566d
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-28 13:03:38 +0530

    Update updater.py

--- DYNAMIC/plugins/updater.py
+++ DYNAMIC/plugins/updater.py
@@ -37,13 +37,7 @@ import os
 import sys
 import asyncio
 
-# -- Constants -- #
-IS_SELECTED_DIFFERENT_BRANCH = (
-    "looks like a custom branch {branch_name} "
-    "is being used:\n"
-    "in this case, Updater is unable to identify the branch to be updated."
-    "please check out to an official branch, and re-start the updater."
-)
+
 OFFICIAL_UPSTREAM_REPO = "https://github.com/TeamDynamic/Dynamic-Userbot"
 BOT_IS_UP_TO_DATE = "`The userbot is up-to-date.\nThank you for Using this Service.`"
 NEW_BOT_UP_DATE_FOUND = (
