bd30dc1f33c36a03e3744b34f0f3fef8c6fe25b5
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-29 08:34:01 +0530

    Update pmpermit.py

--- DYNAMIC/plugins/pmpermit.py
+++ DYNAMIC/plugins/pmpermit.py
@@ -1,5 +1,5 @@
 # Woking
-# pmpermit for HellBot.....
+# pmpermit 
 
 import asyncio
 import io
@@ -10,7 +10,7 @@ from telethon import events, functions
 from telethon.tl.functions.users import GetFullUserRequest
 
 from DYNAMIC.plugins.sql_helper import pmpermit_sql as pmpermit_sql
-from DYNAMIC import ALIVE_NAME, CUSTOM_PMPERMIT, DYNAMIC_ID
+from DYNAMIC import ALIVE_NAME, DYNAMIC_ID
 from DYNAMIC import DYNAMICConfig
 from DYNAMIC.utils import admin_cmd
 from DYNAMIC.cmdhelp import CmdHelp
