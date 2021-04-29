20610207f28978a293ab5a79a4f7c858fd1e2407
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-29 08:28:51 +0530

    Update pmpermit.py

--- DYNAMIC/plugins/pmpermit.py
+++ DYNAMIC/plugins/pmpermit.py
@@ -228,11 +228,11 @@ if Var.PRIVATE_GROUP_ID is not None:
         chat_id = event.sender_id
 
         message_text.lower()
-        if USER_BOT_NO_WARN == message_text:
-           userbotbot's should not reply to other userbot's
+        #if USER_BOT_NO_WARN == message_text:
+         #  userbotbot's should not reply to other userbot's
             # https://core.telegram.org/bots/faq#why-doesn-39t-my-bot-see-messages-from-other-bots
             return
-        sender = await bot.get_entity(chat_id)
+        #sender = await bot.get_entity(chat_id)
 
         if chat_id == bot.uid:
 
