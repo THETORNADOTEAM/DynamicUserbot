aff4d3fed4f8a0f000d83b500d2a42eaa6ec8f92
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-08 20:46:58 +0530

    Update __main__.py

--- DYNAMIC/__main__.py
+++ DYNAMIC/__main__.py
@@ -54,7 +54,22 @@ for name in files:
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
 
+if  EXTRA_PLUGS == True:
+    os.system("git clone https://github.com/TeamDynamic/DYNAMIC-PLUGINS.git ./ULTRA/plugins/")
+    path = "ULTRA/plugins/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as a:
+            patt = Path(a.name)
+            plugin_name = patt.stem
+            try:
+                load_module(plugin_name.replace(".py", ""))
+                if not plugin_name.startswith("__") or plugin_name.startswith("_"):
+                    print ('INSTALLING ALL MODULES', plugin_name)
+            except:
+                pass
 
+else:
 path = 'DYNAMIC/plugins/*.py'
 files = glob.glob(path)
 for name in files:
