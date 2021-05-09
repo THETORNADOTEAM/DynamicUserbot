95b9da67590216d4c3704b374f17d5bb534e7526
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-09 21:18:33 +0530

    ASSISTANT

--- DYNAMIC/__main__.py
+++ DYNAMIC/__main__.py
@@ -11,11 +11,6 @@ from DYNAMIC import LOAD_PLUG, BOTLOG_CHATID, LOGS
 from pathlib import Path
 import asyncio
 import telethon.utils
-EXTRA_PLUGS = os.environ.get("EXTRA_PLUGS", True)
-async def add_bot(bot_token):
-    await bot.start(bot_token)
-    bot.me = await bot.get_me() 
-    bot.uid = telethon.utils.get_peer_id(bot.me)
 
 async def add_bot(bot_token):
     await bot.start(bot_token)
@@ -41,27 +36,19 @@ else:
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
         print("Loading Software Completed")
         print("LOADING HIDDEN FILES")
+        print ('INSTALLING ALL STABLE VERSION OF DYNAMIC USERBOT AND PLUGINS', plugin_name)
     else:
         bot.start()
-    
-
-import glob
-if  EXTRA_PLUGS == True:
-    os.system("git clone https://github.com/TeamDynamic/DYNAMIC-PLUGINS.git")
-    path = "*.py"
-    files = glob.glob(path)
-    for name in files:
-        with open(name) as a:
-            patt = Path(a.name)
-            plugin_name = patt.stem
-            try:
-                load_module(plugin_name.replace(".py", ""))
-                if not plugin_name.startswith("__") or plugin_name.startswith("_"):
-                    print ('INSTALLING ALL APPS AND PLUGINS', plugin_name)
-            except:
-                pass
-
-else:
+   
+import glob   
+ path = 'ULTRA/plugins/assistant/*.py'
+ files = glob.glob(path)
+ for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_pro(shortname.replace(".py", ""))
+      
   path = 'DYNAMIC/plugins/*.py'
   files = glob.glob(path)
   for name in files:
@@ -74,7 +61,9 @@ import DYNAMIC._core
 
 print("DYNAMIC LOADED HIDDEN FILES, SOFTWARE WITH SUCESS JOIN SUPPORT FOR MORE INFO @DYNAMICUSERBOTSUPPORT ")
 print("SOFTWARE VERSION 1.0 Stable")
+print("DYNAIMIC BRANCH: Stable")
 print(" TELETHON VERSION 1.21.1 ")
+print("DYNAMIC BETA USERBOT AND DEVELOPER VERSION COMING SOON")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
