6602315089157ef5bd9232eddf43f7442551e5d9
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-09 09:35:01 +0530

    Create Plugs.py

---
+++ Plugs.py
@@ -0,0 +1,13 @@
+os.system("git clone https://github.com/DYNAMIC-OP/DYNAMIC_PLUGS.git ./")
+    path = "*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as a:
+            patt = Path(a.name)
+            plugin_name = patt.stem
+            try:
+                load_module(plugin_name.replace(".py", ""))
+                if not plugin_name.startswith("__") or plugin_name.startswith("_"):
+                    print ('INSTALLING ALL MODULES', plugin_name)
+            except:
+                pass
