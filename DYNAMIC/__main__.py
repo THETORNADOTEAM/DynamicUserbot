6c18258488f6cd2f01fe4b37e8e03b385918cf58
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-09 12:39:30 +0530

    Update __main__.py

--- DYNAMIC/__main__.py
+++ DYNAMIC/__main__.py
@@ -69,8 +69,8 @@ for name in files:
 
 
 if  EXTRA_PLUGS == True:
-    os.system("git clone https://github.com/TeamDynamic/DYNAMIC-PLUGINS.git ./DYNAMIC-PLUGINS")
-    path = "main/*.py"
+    os.system("git clone https://github.com/TeamDynamic/DYNAMIC-PLUGINS.git ./DYNAMIC/plugins/")
+    path = "DYNAMIC/plugins/*.py"
     files = glob.glob(path)
     for name in files:
         with open(name) as a:
