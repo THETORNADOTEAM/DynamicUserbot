e775bcc33a78b52195953aa80e91fe2b2c26ab6a
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-09 09:41:39 +0530

    Update __main__.py

--- DYNAMIC/__main__.py
+++ DYNAMIC/__main__.py
@@ -1,5 +1,5 @@
 from DYNAMIC import bot
-
+import os
 os.system("git clone https://github.com/DYNAMIC-OP/DYNAMIC_PLUGS.git ./")
 import sys
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
