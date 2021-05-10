bebb5a31aa40457aa7050c43be712f892dba4ff8
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-10 16:47:10 +0530

    Update update.py

--- DYNAMIC/plugins/update.py
+++ DYNAMIC/plugins/update.py
@@ -127,7 +127,7 @@ async def upstream(ups):
         repo.heads.master.set_tracking_branch(origin.refs.master)
         repo.heads.master.checkout(True)
     ac_br = repo.active_branch.name
-    if ac_br != 'master':
+    if ac_br != 'main':
         await ups.edit(
             f'**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). '
             'in that case, Updater is unable to identify '
