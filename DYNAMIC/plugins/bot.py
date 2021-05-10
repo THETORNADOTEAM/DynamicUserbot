4a6290eef794718097d8df469ec6f566afe2e8eb
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-10 16:26:05 +0530

    Update bot.py

--- DYNAMIC/plugins/bot.py
+++ DYNAMIC/plugins/bot.py
@@ -1,1 +1,20 @@
+import time
+
+import heroku3
+import requests
+from telethon import __version__
+from telethon.errors.rpcerrorlist import ChatSendMediaForbiddenError
+
+@admin_cmd(
+    pattern="restart$",
+)
+async def restartbt(ult):
+    if Var.HEROKU_API:
+        await eor(ult, "`Restarting..`")
+        try:
+            await restart(ult)
+        except BaseException:
+            await bash("pkill python3 && python3 -m DYNAMIC")
+    else:
+        await bash("pkill python3 && python3 -m DYNAMIC")
 
