289ae17f7f8c07f96dba7ab4a4e472d57e9f3455
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-10 17:03:00 +0530

    ok

--- DYNAMIC/plugins/x.py
+++ DYNAMIC/plugins/x.py
@@ -24,7 +24,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             if event.query.user_id == bot.uid or event.query.user_id == id:
                 current_page_number=0
                 dc = paginate_help(current_page_number, CMD_LIST, "helpme")
-                await event.edit("`>>>\n\nOᴘᴇɴᴇᴅ ᴛʜᴇ Mᴀɪɴ Mᴇɴᴜ ғᴏʀ \n© υℓтяα χ` ", buttons=dc)
+                await event.edit("`>>>\n\nOᴘᴇɴᴇᴅ ᴛʜᴇ Mᴀɪɴ Mᴇɴᴜ ғᴏʀ \n© DYNAMIC USERBOT` ", buttons=dc)
             else:
                 reply_pop_up_alert = "Pʟᴇᴀsᴇ Gᴇᴛ Yᴏᴜʀ Oᴡɴ Usᴇʀʙᴏᴛ (☞ ͡° ͜ʖ ͡°)☞ Fᴏʀ Mᴏʀᴇ ɪɴғᴏ Vɪsɪᴛ @DYNAMICX_Support !!"
                 await event.answer(reply_pop_up_alert, alert=True)
@@ -39,7 +39,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         if event.query.user_id == bot.uid and query.startswith("Userbot") or event.query.user_id == id and query.startswith("Userbot"):
             rev_text = query[::-1]
             dc = paginate_help(0, CMD_LIST, "helpme")
-            result = builder.article("© υℓтяα χ Hᴇʟᴘ",text="υℓтяα χ Hᴇʟᴘ\n\nCᴜʀʀᴇɴᴛʟʏ ʟᴏᴀᴅᴇᴅ Pʟᴜɢɪɴs: **{}**".format( len(CMD_LIST)),buttons=dc,link_preview=False)
+            result = builder.article("© DYNAMIC Hᴇʟᴘ",text="DYNAMIC Hᴇʟᴘ\n\nCᴜʀʀᴇɴᴛʟʏ ʟᴏᴀᴅᴇᴅ Pʟᴜɢɪɴs: **{}**".format( len(CMD_LIST)),buttons=dc,link_preview=False)
             await event.answer([result] if result else None)
         else:
               reply_pop_up_alert = "Pʟᴇᴀsᴇ Gᴇᴛ Yᴏᴜʀ Oᴡɴ Usᴇʀʙᴏᴛ (☞ ͡° ͜ʖ ͡°)☞ Fᴏʀ Mᴏʀᴇ ɪɴғᴏ Vɪsɪᴛ @DYNAMICX_Support !!"
