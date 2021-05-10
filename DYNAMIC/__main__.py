a9247f2906dfe9a57421f1c7b13ef7004b2fd02a
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-10 09:55:25 +0530

    Update __main__.py

--- DYNAMIC/__main__.py
+++ DYNAMIC/__main__.py
@@ -43,13 +43,13 @@ else:
     else:
         bot.start()
 import glob
-    path = "DYNAMIC/plugins/assistant*.py"
-    files = glob.glob(path)
-    for name in files:
-        with open(name) as f:
-            path1 = Path(f.name)
-            shortname = path1.stem
-            start_assistant(shortname.replace(".py", ""))
+path = 'DYNAMIC/plugins/assistant/*.py'
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
 else:
     print("Assitant is Not Loading As U Have Disabled")    
 
