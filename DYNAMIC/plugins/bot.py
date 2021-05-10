c94076c5109f39458709a4f1e10a75d63adbae00
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-10 16:27:03 +0530

    Update bot.py

--- DYNAMIC/plugins/bot.py
+++ DYNAMIC/plugins/bot.py
@@ -5,6 +5,22 @@ import requests
 from telethon import __version__
 from telethon.errors.rpcerrorlist import ChatSendMediaForbiddenError
 
+from . import *
+
+HEROKU_API = None
+HEROKU_APP_NAME = None
+
+try:
+    if Var.HEROKU_API and Var.HEROKU_APP_NAME:
+        HEROKU_API = Var.HEROKU_API
+        HEROKU_APP_NAME = Var.HEROKU_APP_NAME
+        Heroku = heroku3.from_key(Var.HEROKU_API)
+        heroku_api = "https://api.heroku.com"
+        app = Heroku.app(Var.HEROKU_APP_NAME)
+except BaseException:
+    HEROKU_API = None
+    HEROKU_APP_NAME = None
+
 @admin_cmd(
     pattern="restart$",
 )
