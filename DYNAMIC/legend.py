adf08fdb78926626aa904d44ce2be149267f4be4
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-11 17:20:44 +0530

    OK

---
+++ DYNAMIC/legend.py
@@ -0,0 +1,38 @@
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from userbot import ALIVE_NAME, StartTime
+from userbot.utils import admin_cmd
+from userbot import bot
+from telethon import version
+from math import ceil
+import json
+import random
+import re
+from telethon import events, errors, custom
+import io
+from userbot.helpers.functions import get_readable_time
+import time
+import os
+import datetime
+#importing finished
+from userbot import botnickname 
+BOT = str(botnickname) if botnickname else "DYNAMIC USERBOT"
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "DYNAMIC USER"
+tim = get_readable_time((time.time() - StartTime))
+#pic 👇
+PIC = os.environ.get("ALIVE_PIC")
+uptime = tim
+#time = date + time okay
+TIME = time.asctime(time.localtime())
+#my name 👇
+DYNAMIC = "[DYNAMIC](https://t.me/OFFLINEMODEON)"
+#my bots repo 👇
+REPO = "[LEGEND BOT](https://github.com/TeamDynamic/Dynamic-Userbot)"
+global ghanti
+X = bot.uid
+MASTER = f"[{NAME}](tg://user?id={X})"
+GROUP = "[SUPPORT GROUP](https://t.me/DYNAMICUSERBOTSUPPORT)"
+ALIVE = "DYNAMIC USERBOT IS ON 🔥 FIRE 🔥" #make by LEGENDX22
+OP = " HELLO MASTER MY NAME IS DYNAMIC I AM A BEST USERBOT 💝"
+EMOJI = "🔥"
