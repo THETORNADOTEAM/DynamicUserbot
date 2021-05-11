e6a3eecdae7e7b8080f2a56db2ef79bcfc3847d4
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-11 18:56:12 +0530

    OKAY

--- DYNAMIC/plugins/clone.py
+++ DYNAMIC/plugins/clone.py
@@ -6,6 +6,7 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 from DYNAMIC.utils import admin_cmd
+from DYNAMIC import BOT_LOG
 from telethon.tl import functions
 from telethon import events
 from telethon.errors import ImageProcessFailedError, PhotoCropSizeSmallError
