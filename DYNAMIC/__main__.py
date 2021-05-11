85e75196b1f06c7f969a56e927680e46cb7fdb97
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-11 16:45:19 +0530

    Update __main__.py

--- DYNAMIC/__main__.py
+++ DYNAMIC/__main__.py
@@ -62,6 +62,16 @@ for name in files:
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
 
+import glob
+
+path = 'userbot/plugins/*.py'
+files = glob.glob(path)
+for name in files:
+    with open(name) as f:
+        path1 = Path(f.name)
+        shortname = path1.stem
+        load_module(shortname.replace(".py", ""))
+
 import DYNAMIC._core
 
 print("DYNAMIC LOADED HIDDEN FILES, SOFTWARE WITH SUCESS JOIN SUPPORT FOR MORE INFO @DYNAMICUSERBOTSUPPORT ")
