fae3f34abcc408ee50d2eec0c02fd79cff69857b
Team Dynamic <72003733+amanpandey7647@users.noreply.github.com>
2021-05-16 20:00:31 +0530

    Update main_assistant.py

--- DYNAMIC/plugins/assistant/main_assistant.py
+++ DYNAMIC/plugins/assistant/main_assistant.py
@@ -127,10 +127,8 @@ async def all_messages_catcher(event):
 
 @tgbot.on(events.NewMessage(func=lambda e: e.is_private))
 async def sed(event):
-    msg = await send_message()
     msg.id
     msg_s = event.raw_text
-    user_id, reply_message_id = his_userid(msg.id)
     if event.sender_id == bot.uid:
         if event.raw_text.startswith("/"):
             pass
