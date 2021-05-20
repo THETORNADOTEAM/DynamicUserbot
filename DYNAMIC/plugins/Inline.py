dc341eb27a0bcad6a137363978fd32d349d845eb
Team Dynamic <72003733+amanpandey7647@users.noreply.github.com>
2021-05-20 17:01:53 +0530

    Restart

--- DYNAMIC/plugins/Inline.py
+++ DYNAMIC/plugins/Inline.py
@@ -57,7 +57,7 @@ async def wisper(event):
 
 from telethon import events, Button, custom
 import os,re
-from ULTRAX import ID
+from DYNAMIC import ID
 from telethon.tl.custom import Button 
 from telethon import events, errors, custom, functions
 @tgbot.on(events.InlineQuery(pattern=r"restart"))
