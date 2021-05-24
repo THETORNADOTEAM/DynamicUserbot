8145ddce66832035bd1d504ab3611324312abf36
Team Dynamic <72003733+amanpandey7647@users.noreply.github.com>
2021-05-24 09:17:36 +0530

    Update __init__.py

--- DYNAMIC/plugins/__init__.py
+++ DYNAMIC/plugins/__init__.py
@@ -5,4 +5,6 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 os.system("pip install AmanPandey")
 from AmanPandey import *
-from AmanPandey.utils import admin_cmd, sudo_cmd
+from AmanPandey.utils import admin_cmd, sudo_cmd, load_module, humanbytes, register, command, decorator, start_assistant
+from AmanPandey.config import Config
+from AmanPandey.variables import Var
