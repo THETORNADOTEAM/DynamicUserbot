1364d0792be978aeeb73309d4eab49c74be791d1
Team Dynamic <72003733+amanpandey7647@users.noreply.github.com>
2021-05-24 08:59:51 +0530

    Update __init__.py

--- DYNAMIC/plugins/__init__.py
+++ DYNAMIC/plugins/__init__.py
@@ -4,5 +4,5 @@ import asyncio
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 os.system("pip install Extreme-Pro")
-from Extreme-Pro import *
-from Extreme-Pro.utils import admin_cmd
+from ExtremePro import *
+from ExtremePro.utils import admin_cmd
