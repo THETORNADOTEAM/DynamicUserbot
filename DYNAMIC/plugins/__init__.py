f47203537a622fc44a50c122bdf675286dff54de
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-30 18:31:17 +0530

    init

--- DYNAMIC/plugins/__init__.py
+++ DYNAMIC/plugins/__init__.py
@@ -3,6 +3,5 @@ import os
 import asyncio
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-os.system("pip install Extre")
 from var import Var
 from DYNAMIC import *
