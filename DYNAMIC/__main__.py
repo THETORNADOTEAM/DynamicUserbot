68befed30e875cc67e433124c9fccd3520962cf7
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-30 18:50:52 +0530

    temst

--- DYNAMIC/__main__.py
+++ DYNAMIC/__main__.py
@@ -43,24 +43,6 @@ else:
         print ('INSTALLING ALL STABLE VERSION OF DYNAMIC USERBOT AND PLUGINS')
     else:
         bot.start()
-import glob
-path = 'DYNAMIC/plugins/assistant/*.py'
-files = glob.glob(path)
-for name in files:
-    with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        start_assistant(shortname.replace(".py", ""))   
-
-#import glob
-
-#path = 'DYNAMIC/plugins/*.py'
-#files = glob.glob(path)
-#for name in files:
-#    with open(name) as f:
-#        path1 = Path(f.name)
-#        shortname = path1.stem
-#        load_module(shortname.replace(".py", ""))
 
 import glob
 
